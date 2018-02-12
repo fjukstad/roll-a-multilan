2ce7ea77e301b3f5ae69c0437b7e554766f77009
Bjørn Fjukstad <bjorn@cs.uit.no>
2018-02-12 16:57:31 +0100

    here we go!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	private Rigidbody rb; 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rb.AddForce(movement * speed); 
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Roll-A-MultiLAN
+Starter code for Unity competition at [MultiLAN 2018](http://multilan.no). 
